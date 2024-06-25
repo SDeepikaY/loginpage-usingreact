a516ad7ad42fa2605ae5b06fe2fa3b25d44dca05
Sai Deepika Yarlagadda <saideepikay@Sais-MacBook-Air.local>
2024-06-25 08:00:01 -0700

    peoject without css

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 
 
-export const Login = () => {                      /*rafce- to create initial basic component */
+export const Login = (props) => {                      /*rafce- to create initial basic component */
   const [email,setEmail]=useState('');
   const [password,setPassword]=useState('');  
   
@@ -14,16 +14,16 @@ export const Login = () => {                      /*rafce- to create initial bas
   
            
   return (
-    <>
+    <div className='auth-form-container'>
       <form onSubmit={{handleSubmit}}>
-        <label for='email' >email</label>
+        <label htmlFor='email' >email</label>
         <input value={email} type='email' placeholder='enter your email' id='email' name='email'/>
-        <label for ='password'>password</label>
+        <label htmlFor ='password'>password</label>
         <input value={password} type='password' placeholder='enter your password' id='password' name='password'/>
         <button type='submit'>login</button>
       </form>
-      <button>Dont have have an account? Register here</button>
-    </>
+      <button onClick={()=>props.onFormSwitch('register')}>Dont have have an account? Register here</button>
+    </div>
       
   )
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export const Register = () => {
+export const Register = (props) => {
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState(''); 
+  const [fullname,setfullname]=useState('');
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log()
+  }
+
   return (
-    // <div>Register</div>
-    <>Register</>
+    <div className='auth-form-container'>
+    <form onSubmit={{handleSubmit}}>
+      <label htmlFor='email'>email</label>
+      <input value={email} type='email' placeholder='enter email' id='email' name='email'/>
+      <label htmlFor='password'>password</label>
+      <input value={password} type='password' placeholder='set your password' id='password' name='password'/>
+      <label htmlFor='fullname'>fullname</label>
+      <input value={fullname} type='fullname' placeholder='enter your full name' id='fullname' name='fullname'/>
+    </form>
+    <button onClick={()=>props.onFormSwitch('login')}>Already have an account! Login here</button>
+    </div>
   )
 }
