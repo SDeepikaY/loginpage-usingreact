2bb6a8a1df9a4631b68e47c09315d3d1dfdc2575
Sai Deepika Yarlagadda <saideepikay@Sais-MacBook-Air.local>
2024-06-25 08:28:55 -0700

    with css

--- src/Login.jsx
+++ src/Login.jsx
@@ -15,14 +15,15 @@ export const Login = (props) => {                      /*rafce- to create initia
            
   return (
     <div className='auth-form-container'>
-      <form onSubmit={{handleSubmit}}>
+      <h2>Login</h2>
+      <form className='login-form' onSubmit={{handleSubmit}}>
         <label htmlFor='email' >email</label>
         <input value={email} type='email' placeholder='enter your email' id='email' name='email'/>
         <label htmlFor ='password'>password</label>
         <input value={password} type='password' placeholder='enter your password' id='password' name='password'/>
         <button type='submit'>login</button>
       </form>
-      <button onClick={()=>props.onFormSwitch('register')}>Dont have have an account? Register here</button>
+      <button className='link-btn' link- onClick={()=>props.onFormSwitch('register')}>Dont have have an account? Register here</button>
     </div>
       
   )
